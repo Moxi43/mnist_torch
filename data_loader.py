4dfbe89b91db755bf6bdde969f162f04bee9c470
Moxi43 <vostretsov0505@gmail.com>
2021-06-12 21:21:30 +0400

    Two new files were added and one changed.
    Changes in data_loader:
        -Added data transforms with pytorch method.
    
    Changes in net:
        -Added super().
    
    New file test_train:
        -There will be two functions in the file (train and test), there is
         only train function in this commit.

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,14 @@
 from torchvision.datasets import MNIST
-
+from os.path import isdir
+from torchvision import transforms
 
 def getData(x: bool):
-    return MNIST(root='./data', train=x, download = True, transform = None)
-   
+
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+    if isdir("./data"):
+        return MNIST(root='./data', train=x, download = False, transform = transform)
+    else:
+        return MNIST(root='./data', train=x, download = True, transform = transform)
